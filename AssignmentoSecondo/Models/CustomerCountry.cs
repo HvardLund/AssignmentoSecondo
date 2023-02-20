ca2fbeb730bb90e86e5aab0f8c56534ea503bc8c
Håvard Lund <havard.lund@no.experis.com>
2023-02-20 14:35:32 +0100

    feat: add models for customer, customerCountry, customerGenre and customerSpender

---
+++ AssignmentoSecondo/Models/CustomerCountry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentoSecondo.Models
+{
+    internal class CustomerCountry
+    {
+        public string Country { get; set; }
+        public int NumberOfCustomers { get; set; }
+    }
+}
---
+++ AssignmentoSecondo/Models/CustomerGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentoSecondo.Models
+{
+    internal class CustomerGenre
+    {
+        public string Name { get; set; }
+        public int PopularGenre { get; set; }
+    }
+}
